f9e8b718c3ef40bba6ba871894c01279474ef0b0
Yipeng Sun <syp@umd.edu>
2021-04-11 23:36:28 +0200

    Move file path translator into a separate function.

--- TrackerOnlyEmu/loader.py
+++ TrackerOnlyEmu/loader.py
@@ -2,16 +2,22 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Wed Mar 31, 2021 at 04:11 PM +0200
+# Last Change: Sun Apr 11, 2021 at 11:36 PM +0200
 
 from os import path
 from ROOT import gInterpreter
 
 
-def load_cpp(filepath, current_file_path=__file__):
+def load_file(filepath, current_file_path=__file__):
     if filepath.startswith('<') and filepath.endswith('>'):
         filepath = path.join(path.abspath(path.dirname(current_file_path)),
                              filepath[1:-1])
+
+    return filepath
+
+
+def load_cpp(filepath, current_file_path=__file__):
+    filepath = load_file(filepath, current_file_path)
 
     # Now load the CPP file in ROOT
     with open(filepath, 'r') as f:
