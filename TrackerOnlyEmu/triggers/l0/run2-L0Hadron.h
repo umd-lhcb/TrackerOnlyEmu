aad393efd026494aed5b7e5e9b235e5ea71c407c
Yipeng Sun <syp@umd.edu>
2021-04-19 03:00:26 +0200

    WIP #3: Reimplement all L0 C++ functions in Simone's code.

--- TrackerOnlyEmu/triggers/l0/run2-L0Hadron.h
+++ TrackerOnlyEmu/triggers/l0/run2-L0Hadron.h
@@ -1,6 +1,6 @@
 // Stolen from:
 //   https://gitlab.cern.ch/lhcb-slb/B02DplusTauNu/-/blob/master/tuple_processing_chain/emulate_L0Hadron_TOS_RLc.py
-// Last Change: Mon Apr 19, 2021 at 12:58 AM +0200
+// Last Change: Mon Apr 19, 2021 at 03:00 AM +0200
 //
 #ifndef _RUN2_L0_HADRON_
 #define _RUN2_L0_HADRON_
@@ -9,6 +9,7 @@
 
 #include <TFile.h>
 #include <TH1D.h>
+#include <TMath.h>
 #include <TRandom3.h>
 #include <TString.h>
 
@@ -25,6 +26,8 @@ const double P_LOW   = 0;
 const double P_HIGH  = 1e5;
 const double PT_LOW  = 0;
 const double PT_HIGH = 15000;
+
+TRandom3 SHARED_EFF = TRandom3( 41 );
 
 ///////////////////////////////////
 // Single particle HCAL response //
@@ -56,8 +59,8 @@ int computeRespBin( double x, double xLow, double xHigh, int numBins ) {
 }
 
 // Reimplementation of 'random_smearing'
-double singlePartEtSmear( double P, double PT, double realET,
-                          vector<vector<TH1D*> > respHistos ) {
+double singlePartEt( double P, double PT, double realET,
+                     vector<vector<TH1D*> > respHistos ) {
   auto binP  = computeRespBin( P, P_LOW, P_HIGH, P_BIN );
   auto binPT = computeRespBin( PT, PT_LOW, PT_HIGH, PT_BIN );
   auto hist  = respHistos[binP][binPT];
@@ -67,6 +70,48 @@ double singlePartEtSmear( double P, double PT, double realET,
   if ( smearedET > 6100 ) smearedET = 6100;  // Due to limitation of HCAL
 
   return smearedET;
+}
+
+//////////////////////////////
+// Two particle corrections //
+//////////////////////////////
+
+double rDiff( double x1, double y1, double x2, double y2 ) {
+  return TMath::Sqrt( TMath::Power( x1 - x2, 2 ) +
+                      TMath::Power( y1 - y2, 2 ) );
+}
+
+bool isShared( double rDiff, int region1, int region2, TH1D* histoSharedInner,
+               TH1D* histoSharedOuter ) {
+  if ( region1 != region2 ) return false;
+
+  TH1D* histo = histoSharedOuter;
+  if ( region1 == 1 ) histo = histoSharedInner;
+
+  double fracShared = histo->GetBinContent( histo->FindBin( rDiff ) );
+  if ( SHARED_EFF.Uniform() < fracShared ) return true;
+  return false;
+}
+
+double missingFraction( double rDiff, int region1, int region2,
+                        TH1D* histoMissingInner, TH1D* histoMissingOuter ) {
+  if ( region1 != region2 ) return 0;
+
+  TH1D* histo = histoMissingOuter;
+  if ( region1 == 1 ) histo = histoMissingInner;
+
+  auto   bin      = histo->FindBin( rDiff );
+  auto   totFrac  = histo->GetBinContent( P_BIN );
+  double missFrac = histo->GetBinContent( bin ) / totFrac;
+  return missFrac;
+}
+
+// Reimplementation of 'calcET' and 'calcDplusET'
+double twoPartEt( double smearedET1, double smearedET2, bool isShared,
+                  double missFrac ) {
+  double ET = smearedET1;
+  if ( isShared ) ET = ( ET + smearedET2 ) * missFrac;
+  return ET;
 }
 
 #endif
