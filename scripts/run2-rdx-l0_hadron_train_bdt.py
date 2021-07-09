74f85757ecafd77961661c3f7bb84f4ed0d1a8d7
Yipeng Sun <syp@umd.edu>
2021-07-10 01:56:47 +0200

    WIP #3: Make sure debug ntuple is generated w/ training sample only.

--- scripts/run2-rdx-l0_hadron_train_bdt.py
+++ scripts/run2-rdx-l0_hadron_train_bdt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # Author: Yipeng Sun
-# Last Change: Thu Jul 08, 2021 at 08:47 PM +0200
+# Last Change: Sat Jul 10, 2021 at 01:39 AM +0200
 # Based on the script 'regmva.py' shared by Patrick Owen
 
 import pickle
@@ -40,6 +40,13 @@ REGRESSION_BRANCHES = [
     EXEC('Define', 'd0_trg_et',
          'capHcalResp(k_L0Calo_HCAL_TriggerET, pi_L0Calo_HCAL_TriggerET)', True),
     EXEC('Define', 'd0_et_diff', 'd0_trg_et - d0_et_emu_no_bdt', True),
+]
+
+# Keep these branches in output ntuples for debugging
+DEBUG_BRANCHES = [
+    'runNumber',
+    'eventNumber',
+    'd0_et_emu_no_bdt'
 ]
 
 
@@ -82,6 +89,18 @@ specify test ntuple output location.''')
 load_cpp('<triggers/l0/run2-L0Hadron.h>')
 
 
+###########
+# Helpers #
+###########
+
+def gen_output_dict(arr, names=BDT_TRAIN_BRANCHES+DEBUG_BRANCHES):
+    return dict(zip(names, arr.T))
+
+
+def slice_bdt_input(arr, right_idx=len(BDT_TRAIN_BRANCHES)):
+    return arr[:, 0:right_idx]
+
+
 #############
 # Train BDT #
 #############
@@ -91,20 +110,21 @@ if __name__ == '__main__':
 
     init_frame = RDataFrame(args.tree, args.input)
 
-    bdt_input_vars = np.array(
-        list(init_frame.AsNumpy(columns=BDT_TRAIN_BRANCHES).values())).T
+    input_vars = np.array(
+        list(init_frame.AsNumpy(
+            columns=BDT_TRAIN_BRANCHES+DEBUG_BRANCHES).values())).T
     idx_vars = np.array(
         list(init_frame.AsNumpy(columns=['runNumber', 'eventNumber']))).T
 
     dfs, output_br_names = process_directives(REGRESSION_BRANCHES, init_frame)
     regression_var = dfs[-1].AsNumpy(columns=['d0_et_diff'])
 
-    bdt_input_vars_dev, _, regression_var_dev, _ = train_test_split(
-        bdt_input_vars, regression_var['d0_et_diff'],
+    input_vars_dev, _, regression_var_dev, _ = train_test_split(
+        input_vars, regression_var['d0_et_diff'],
         test_size=0.2, random_state=42)
-    bdt_input_vars_train, bdt_input_vars_test, \
+    input_vars_train, input_vars_test, \
         regression_var_train, regression_var_test = train_test_split(
-            bdt_input_vars_dev, regression_var_dev, test_size=0.2,
+            input_vars_dev, regression_var_dev, test_size=0.2,
             random_state=492)
 
     if not args.load_bdt:
@@ -113,7 +133,9 @@ if __name__ == '__main__':
         rng = np.random.RandomState(1)
         bdt = AdaBoostRegressor(DecisionTreeRegressor(max_depth=args.max_depth),
                                 n_estimators=300, random_state=rng)
-        bdt.fit(bdt_input_vars_train, regression_var_train)
+
+        bdt_input = slice_bdt_input(input_vars_train)
+        bdt.fit(bdt_input, regression_var_train)
         time_stop = time.perf_counter()
         print('BDT fitted. It takes a total of {:,.2f} sec'.format(
             time_stop - time_start))
@@ -129,28 +151,26 @@ if __name__ == '__main__':
 
     if args.debug_ntuple is not None:
         print('Generate debug ntuple...')
-        debug_output = dfs[-1].AsNumpy()
-        debug_output['d0_et_diff_pred'] = bdt.predict(bdt_input_vars)
-
-        # Delete the d0_L0HadronDecision_TOS branch as we can't convert from a
-        # numpy bool array to a TTree one directly.
-        del(debug_output['d0_L0HadronDecision_TOS'])
+        debug_output = gen_output_dict(input_vars_train)
+        debug_output['d0_et_diff'] = regression_var_train
+        debug_output['d0_et_diff_pred'] = bdt.predict(
+            slice_bdt_input(input_vars_train))
 
         debug_rdf = ROOT.RDF.MakeNumpyDataFrame(debug_output)
-        final_df = debug_rdf.Define(
+        final_debug_df = debug_rdf.Define(
             'd0_et_trg_pred_diff', 'd0_et_diff - d0_et_diff_pred')
 
-        output_br_names.push_back('d0_et_diff_pred')
-        output_br_names.push_back('d0_et_trg_pred_diff')
-        output_br_names.push_back('d0_et_emu_no_bdt')
-
-        final_df.Snapshot(args.tree, args.debug_ntuple, output_br_names)
+        final_debug_df.Snapshot(args.tree, args.debug_ntuple)
 
     if args.test_ntuple is not None:
         print('Generate test ntuple...')
-        test_output = dict(zip(BDT_TRAIN_BRANCHES, bdt_input_vars_test.T))
+        test_output = gen_output_dict(input_vars_test)
         test_output['d0_et_diff'] = regression_var_test
-        test_output['d0_et_diff_pred'] = bdt.predict(bdt_input_vars_test)
+        test_output['d0_et_diff_pred'] = bdt.predict(
+            slice_bdt_input(input_vars_test))
 
         test_rdf = ROOT.RDF.MakeNumpyDataFrame(test_output)
-        test_rdf.Snapshot(args.tree, args.test_ntuple)
+        final_test_rdf = test_rdf.Define(
+            'd0_et_trg_pred_diff', 'd0_et_diff - d0_et_diff_pred')
+
+        final_test_rdf.Snapshot(args.tree, args.test_ntuple)
