1e94367e6866f2724d405fef2e8ece36ec624ca6
Yipeng Sun <syp@umd.edu>
2021-12-15 18:07:33 +0100

    Bump version.

--- TrackerOnlyEmu/__init__.py
+++ TrackerOnlyEmu/__init__.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Fri Oct 29, 2021 at 02:28 AM +0200
+# Last Change: Wed Dec 15, 2021 at 06:07 PM +0100
 
 __name__ = 'TrackerOnlyEmu'
-__version__ = '0.2.4'  # This version is fixed.
+__version__ = '0.2.5'  # This version is fixed.
