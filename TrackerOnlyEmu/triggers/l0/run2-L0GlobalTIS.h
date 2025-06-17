3083d88584aecbacffbe9ed2a5d20f019b107c29
Alex Fernez <afernez@umd.edu>
2025-06-17 12:50:42 -0400

    add ad-hoc correction for high B pT region where JpsiK L0 Global TIS measurement doesnt do a good job capturing rdx behavior

--- TrackerOnlyEmu/triggers/l0/run2-L0GlobalTIS.h
+++ TrackerOnlyEmu/triggers/l0/run2-L0GlobalTIS.h
@@ -13,16 +13,17 @@
 #include <TString.h>
 
 using std::map;
+using std::vector;
 
 ///////////////////
 // Configurables //
 ///////////////////
 
 const map<int, TString> YEAR_HISTO_REL = {
-    { 2015, "0" },
-    { 2016, "1" },
-    { 2017, "1" },  // FIXME: We don't have TIS response for 2017 and 2018
-    { 2018, "1" },
+    // { 2015, "0" },
+    { 2016, "0" },
+    { 2017, "1" },
+    { 2018, "2" },
 };
 
 const TString RESP_HISTO_PREFIX = "Jpsi_data_eff";
@@ -44,12 +45,31 @@ map<int, TH2F*> readL0GlobalTisResp( TFile* ntp ) {
 
 // This is emulated as a weight in float
 float l0GlobalTisTriggerEmu( double PZ, double PT, int year,
-                             map<int, TH2F*> respHistos ) {
+                             map<int, TH2F*> respHistos, bool adhoc_correction=true ) {
   auto hist = respHistos[year];
+
+  // ad-hoc correction to mimic high log(pT) behavior seen in rdx fullsim MC, found in lhcb-ntuples-gen/scripts/l0_global_tis_highpT_adhoc_correction.py using D*+munu fullsim MC
+  map<int, map<int, vector<float>>> ADHOC_DSTMUNU_HIGHPT_CORRECTION;
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2016][1] = {9.576293217377373, 0.38961711525917053, -0.04307553315370772}; // D*munu high log(pT) bin mean, eff from JpsiK data, slope correction
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2016][2] = {9.645412819275125, 0.40560808777809143, 0.1920121662958972};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2016][3] = {9.724147903446033, 0.41952842473983765, 0.21584737936752765};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2016][4] = {9.80807058838877, 0.46126940846443176, 0.25353211968587264};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2017][1] = {9.573918210303054, 0.3488537669181824, 0.19981855562035766};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2017][2] = {9.648051795507696, 0.3812873661518097, 0.1692287430651749};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2017][3] = {9.724531930069968, 0.42026373744010925, 0.20335972471697286};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2017][4] = {9.80744115981552, 0.4522625207901001, 0.2293965047341547};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2018][1] = {9.575316536210408, 0.36407339572906494, -0.15023259431100983};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2018][2] = {9.64728926210228, 0.3955422341823578, 0.15679897820527733};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2018][3] = {9.72602227412706, 0.4256914556026459, 0.24396464174205812};
+  ADHOC_DSTMUNU_HIGHPT_CORRECTION[2018][4] = {9.807297853596634, 0.45863404870033264, 0.2866896902756077};
 
   if ( PZ > 0 ) {
     auto binPZ = hist->GetXaxis()->FindBin( TMath::Log( PZ ) );
     auto binPT = hist->GetYaxis()->FindBin( TMath::Log( PT ) );
+    if (adhoc_correction && binPT == hist->GetNbinsY()) { // only apply correction to high log(pT) bin (w = w_uncor(a_i(log(pT)-m_i)+e_i)/e_i, but w_uncor = e_i, see l0_global_tis_highpT_adhoc_correction.py for notation)
+      vector<float> adhoc = ADHOC_DSTMUNU_HIGHPT_CORRECTION[year][binPZ];
+      return adhoc[2]*(TMath::Log(PT)-adhoc[0])+adhoc[1];
+    }
     return hist->GetBinContent( binPZ, binPT );
   }
 
